ee2d0c44bc289ea8c344f66ff77b57ce521262d7
Priscilla Alexis Mitchell <alexismitchell42@gmail.com>
2023-01-26 13:46:47 -0600

    update app.js jan26 1:46pm

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-console.log('Hey Dev Ed');
+var life = 100; // This is my life bar
+
+life= life-10;
+//90
+life= life-10;
+//80
+var name= "Dev Ed"; //This is the name displayed in the game STRING
+
+var checkout= false; //BOOLEEAN
+
+var box= null; 
+
+
+console.log(box);
 
